578cba7f5a5c5cb08053bc0a215a74ad5039ccc7
CalebElkinsDev <CalebElkinsDev@outlook.com>
2022-04-25 15:34:33 -0500

    Image loading for details page fixed.

--- app/src/main/java/com/elkins/gamesradar/gamedetails/GameDetailsFragment.kt
+++ app/src/main/java/com/elkins/gamesradar/gamedetails/GameDetailsFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.elkins.gamesradar.R
+import com.elkins.gamesradar.database.DatabaseGame
 import com.elkins.gamesradar.databinding.FragmentGameDetailsBinding
 import com.elkins.gamesradar.gameslist.GamesListViewModel
 import com.elkins.gamesradar.gameslist.GamesListViewModelFactory
@@ -43,5 +45,33 @@ class GameDetailsFragment : Fragment() {
         binding.viewModel = viewModel
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set details in binding layout after the view model has fetched the details from the API
+        viewModel.gameDetails.observe(viewLifecycleOwner) {
+            if(it != null) {
+                initializePlatforms()
+            }
+        }
+    }
+
+    /** Populate the platforms. Used after the game details are fetched */
+    private fun initializePlatforms() {
+        /* Add a custom text view to the platforms group for each platform of the game*/
+
+        for(platform in viewModel.gameDetails.value!!.platforms!!) {
+            val view = LayoutInflater.from(binding.root.context).inflate(
+                R.layout.platform_item,
+                binding.platformsGroup,
+                false
+            ) as TextView
+
+            view.text = platform
+            binding.platformsGroup.addView(view)
+        }
+        binding.executePendingBindings()
     }
 }
--- app/src/main/java/com/elkins/gamesradar/utility/BindingAdapters.kt
+++ app/src/main/java/com/elkins/gamesradar/utility/BindingAdapters.kt
@@ -6,8 +6,7 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.elkins.gamesradar.database.DatabaseGame
-import java.text.SimpleDateFormat
-import java.time.Month
+import com.elkins.gamesradar.gamedetails.GameDetails
 import java.util.*
 
 
@@ -22,7 +21,6 @@ fun fetchImage(view: ImageView, src: String?) {
 //                RequestOptions()
 //                .placeholder()
 //                .error())
-            .thumbnail(0.1f)
             .into(view)
     }
 }
@@ -31,6 +29,46 @@ fun fetchImage(view: ImageView, src: String?) {
 fun TextView.displayReleaseDate(game: DatabaseGame) {
 
     game.run {
+        if(originalReleaseDate > 0) {
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = game.originalReleaseDate
+
+            text = originalReleaseDateDisplayFormat.format(calendar.time)
+
+        } else if(expectedReleaseYear > 0 && expectedReleaseMonth > 0 && expectedReleaseDay > 0) {
+
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.YEAR, expectedReleaseYear)
+            calendar.set(Calendar.MONTH, expectedReleaseMonth)
+            calendar.set(Calendar.DAY_OF_MONTH, expectedReleaseDay)
+
+            text = originalReleaseDateDisplayFormat.format(calendar.time)
+
+        } else if (expectedReleaseMonth > 0 && expectedReleaseYear > 0) {
+
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.MONTH, expectedReleaseMonth)
+
+            val release = "${monthOnlyDateFormat.format(calendar.time)}, $expectedReleaseYear"
+            text = release
+
+        } else if(expectedReleaseQuarter > 0) {
+            val release = "Q$expectedReleaseQuarter, $expectedReleaseYear"
+            text = release
+
+        } else {
+            text = "TBA"
+        }
+    }
+}
+
+// Overloaded method used for GameDetails objects in the details fragment
+@BindingAdapter("releaseDate")
+fun TextView.displayReleaseDate(game: GameDetails?) {
+
+    text = ""
+
+    game?.run {
         if(originalReleaseDate > 0) {
             val calendar = Calendar.getInstance()
             calendar.timeInMillis = game.originalReleaseDate
